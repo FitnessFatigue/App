cb99c5371b67607e375b49ebf1bb95a1a964e6f3
Matthew Roche <matthew@matthewroche.co.uk>
2021-10-07 20:38:48 +0100

    Initial Commit

---
+++ Shared/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  Shared
+//
+//  Created by Matthew Roche on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
